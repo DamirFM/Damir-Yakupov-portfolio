48ee3949f3c6f169b2cabab08a2437cff4495afe
DamirFM <yakupovdamir93@gmail.com>
2024-02-23 17:33:44 -0500

    added header component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css'
+import Header from "@/components/header";
 import { Inter } from 'next/font/google'
 // instantiating the font
 const inter = Inter({ subsets: ['latin'] })
@@ -16,10 +17,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       {/* applying the font inter */}
-      <body className={`${inter.className} bg-gray-50 text-gray-950 relative`}>
+      <body className={`${inter.className} bg-gray-50 text-gray-950 relative h-[5000px]`}>
         <div className="bg-[#fbe2e8] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
-        <div className="bg-[#e7fbd7] absolute top-[-6rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[28rem] xl:left-[15rem] 2xl:left-[-5rem]"></div>
+        <div className="bg-[#d7ecfb] absolute top-[-6rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[28rem] xl:left-[15rem] 2xl:left-[-5rem]"></div>
         {/* page.tsx */}
+        <Header/>
         {children}
         </body>
     </html>
