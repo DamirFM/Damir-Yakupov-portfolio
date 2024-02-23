1d1d9bfd685d25ae64d6331d21a05d20a33a0be5
DamirFM <yakupovdamir93@gmail.com>
2024-02-23 15:07:14 -0500

    update layout.tsx root

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       {/* applying the font inter */}
-      <body className={inter.className}>
+      <body className={`${inter.className} bg-gray-50 text-gray-950 relative`}>
+        <div className="bg-[#fbe2e8] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
+        <div className="bg-[#e7fbd7] absolute top-[-6rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[28rem] xl:left-[15rem] 2xl:left-[-5rem]"></div>
         {/* page.tsx */}
         {children}
         </body>
