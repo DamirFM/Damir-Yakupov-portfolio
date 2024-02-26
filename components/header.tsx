2256f810de65818be58fbeca6c28afaed804dedf
DamirFM <yakupovdamir93@gmail.com>
2024-02-25 20:37:00 -0500

    updated projects component/ added embeded project component

--- components/header.tsx
+++ components/header.tsx
@@ -9,34 +9,34 @@ import Link from "next/link";
 // import { useActiveSectionContext } from "@/context/active-section-context";
 
 export default function Header() {
-  return <header className='z-[999] relative'> 
+  return <header className='z-[999] relative'>
     <motion.div className='fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full'
-    // after applying motion we can use special props
-    // initial state
-    // crating new transform
-    initial={{y: -200, x:'-50%', opacity: 0}}
-    // we will overwrite initial state
-    animate={{y: 0,x:'-50%', opacity: 1}}
+      // after applying motion we can use special props
+      // initial state
+      // crating new transform
+      initial={{ y: -200, x: '-50%', opacity: 0 }}
+      // we will overwrite initial state
+      animate={{ y: 0, x: '-50%', opacity: 1 }}
     ></motion.div>
     <nav className='flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0'>
-        <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
-          {links.map((link) => (
-            <motion.li 
+      <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
+        {links.map((link) => (
+          <motion.li
             // we do not need x:'-50%' here because we have -translate-x-1/2 
-            className="h-3/4 flex items-center justify-center" 
+            className="h-3/4 flex items-center justify-center"
             key={link.hash}
-            initial={{y: -200, opacity: 0}}
-            animate={{y: 0, opacity: 1}}
-            >
-                <Link className="flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition" 
-                href={link.hash}>
-                {link.name}
-                </Link>
-            </motion.li>
+            initial={{ y: -200, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+          >
+            <Link className="flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition"
+              href={link.hash}>
+              {link.name}
+            </Link>
+          </motion.li>
         ))}
-        </ul>
+      </ul>
     </nav>
   </header>;
 
-  
+
 }
---
+++ components/project.tsx
@@ -0,0 +1,76 @@
+"use client";
+import React, { useRef } from 'react'
+import Image from 'next/image'
+import { useScroll, useTransform } from 'framer-motion'
+import { motion } from 'framer-motion'
+
+// lets metion ProjectProps here, by this way we can use it in the Project component
+type ProjectProps = {
+    title: string,
+    description: string,
+    tags: string[],
+    imageUrl: string
+}
+
+export default function Project({
+    title,
+    description,
+    tags,
+    imageUrl
+}: ProjectProps) {
+    // we need to use useRef hook to get the reference to the element
+    const ref = useRef<HTMLElement>(null);
+    const { scrollYProgress } = useScroll({
+        // first we need actial HTML reference to the element
+        target: ref,
+        // then we need to define the viewport offset
+        // this mean when we actually will start the animation
+        // start animation "0 1" mean when the bottom of the element is at the top of the viewport
+        // finish animation "1.33 1" mean when the bottom of the element is at the bottom of the viewport
+        offset: ["0 1", "1.33 1"]
+    })
+    // managing the scale and opacity of the element (I do not want section appear from 0 scale and 0 opacity to 1 scale and 1 opacity, I want to start from 0.8 scale and 0.6 opacity to 1 scale and 1 opacity)
+    // we need to use useTransform hook to transform the scrollYProgress
+    // we need to define the range of the scrollYProgress
+    // we need to define the range of the scaleProgess and opacityProgess
+    const scaleProgess = useTransform(scrollYProgress, [0, 1], [0.8, 1])
+    const opacityProgess = useTransform(scrollYProgress, [0, 1], [0.6, 1])
+
+    return (
+        <motion.div
+            ref={ref}
+            style={{ scale: scaleProgess, opacity: opacityProgess }}
+            className=' group mb-3 sm:mb-8 last:mb-0 '
+        >
+            <section
+                className=' bg-gray-100 max-w-[42rem] rounded-lg border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem] mb-3 sm:mb-8 last:mb-0  hover:bg-gray-200 transition group-even:pl-8'>
+                <div className='pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:ml-[18rem]'>
+                    <h3 className='text-2xl font-semibold'>{title}</h3>
+                    <p className='mt-2 leading-relaxed text-gray-700'>{description}</p>
+                    <ul className='flex flex-wrap mt-4 gap-2 sm:mt-auto'>
+                        {tags.map((tag, index) => (
+                            <li className='bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full' key={index}>{tag}</li>
+                        ))}
+                    </ul>
+                </div>
+
+                <Image
+                    src={imageUrl}
+                    alt={"Project I've worked on"}
+                    quality={95}
+                    className='absolute hidden sm:block top-8 -right-40 w-[28.25rem] rounded-t-lg rounded-b-lg shadow-2xl
+                    transition 
+                    group-hover:scale-[1.04]
+                    group-hover:-translate-x-3
+                    group-hover:translate-y-3
+                    group-hover:-rotate-2
+                
+                    group-even:group-hover:translate-x-3
+                    group-even:group-hover:translate-y-3
+                    group-even:group-hover:rotate-2
+                
+                    group-even:right-[initial] group-even:-left-40' />
+            </section>
+        </motion.div>
+    )
+}
